6296bb8d189506ae5b60c935090bf4e228abf53f
zhengzhisunny <zhengzhi@umich.edu>
2016-12-06 17:52:39 -0500

    one layer RBM Update
    Here except the data treatment, all the other part of the one layer RBM
    is ready

--- rbm.py
+++ rbm.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2.7
 import numpy as np
-<<<<<<< HEAD
 import numpy.random as random
 
 
@@ -51,40 +50,48 @@ hid_con = np.zeros((numcases, numhid))
 data_con = np.zeros((numcases, numhid))
 
 
+# calculate the probability of data or hidden variable
+def func_prob(data, weight_vh, hibias):
+    Ones_m = np.full((numcases,numhid), 1.0)
+    #element inside logistic function
+    X = - np.dots(data, weight_vh) - hibias
+    prob = np.divide(Ones_m, np.add(Ones_m, np.exp(X))    
+    return prob
 
-def func_prob(data_hidprob, data, weight_vh, hibias):
-    for i in range(numcases):    
-        for j in range(numhid):
-            data_hidprob[i][j] = 1./(1+np.exp(-np.dot(data[i,:], weight_vh[:,j])-hibias[0,j]))
-    return data_hidprob
 
+
+# function to use probability to decide the value of data or hidden variable
 def fun_uphid(hid_data, data_hid_prob):
-    for i in range(numcases):
-        for j in range(numhid):
-            if data_hid_prob >= 0.5:
-                hid_data[i,j] = 1
-            else:
-                hid_data[i,j] = 0
+    hid_data = np.rint(data_hid_prob)# return to the nearest integer
     return hid_data
 
+# function to use data and hidden to up data parameters
 def fun_delta_weight(data, hid_data, hid_con, data_con):
-    return epsilonw * (np.dot(data.transpose())
+    delta_weight =epsilonw /(numcases+0.0) * (np.dot(data.transpose(), hid_data) - np.dot(data_con.transpose(), hid_con))
+    return delta_weight
+
+def fun_delta_bias(data, con_data, epsilonvb):
+    return epsilonvb /(data.mean(1) - data_con.mean(2))
+
 
 for iteration in range(n):
     for batch in range(numbatches):
         data = batchdata[:,:,batch]
         #construct hidden layer from data
-        data_hidprob = func_prob(data_hidprob, data, weight_vh, hibias)
+        data_hidprob = func_prob(data, weight_vh, hibias)
         hid_data = fun_uphid(hid_data,data_hidprob)
         #construct confabulation state from hidden layers
-        con_hid_dataprob = func_prob(con_hid_dataprob, hid_data, weight_vh, vibias)
+        con_hid_dataprob = func_prob(hid_data, weight_vh, vibias)
         data_con = fun_uphid(data_con,con_hid_dataprob)
         #construct confabulation hidden state from confabulation data
-        con_data_hidprob = func_prob(con_data_hidprob, data_con, weight_vh, hibias)
+        con_data_hidprob = func_prob(data_con, weight_vh, hibias)
         hid_con = fun_uphid(hid_con, con_data_hidprob)
         # update parameters:
-
-        
+        weight_vh = weight_vh + delta_weight
+        hibias = hibias + delta_hibias
+        vibias = vibias + delta_vibias
+        if delta_weight < 0.001 and hibias < 0.001 and vibias < 0.001:
+            break
         
         
         
