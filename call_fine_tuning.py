227289395e1ef1cc82a9dae09210976c7921a170
shuxuekkl <shenliuxing@gmail.com>
2016-12-14 11:44:03 -0500

    Fine tuning all W update support

--- call_fine_tuning.py
+++ call_fine_tuning.py
@@ -7,39 +7,48 @@ rng = numpy.random
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import time
+start_time = time.time()
 
 # simulation data
-training_steps = 1000
-simulate_data_num = 50
-num_batch = 3
+
+training_steps = 2
+simulate_data_num = 50000
+num_batch = 50
 lr = 0.01
 
-ini_value_w1 = rng.randn(10,8)
-ini_value_w2 = rng.randn(8,6)
-ini_value_w3 = rng.randn(6,4)
-ini_value_w4 = rng.randn(4,2)
-w_list_3_layer = [ini_value_w1, ini_value_w2, ini_value_w3]
-w_list_4_layer = [ini_value_w1, ini_value_w2, ini_value_w3, ini_value_w4]
+ini_value_w1 = rng.randn(784,2000)
+ini_value_w2 = rng.randn(2000,1000)
+ini_value_w3 = rng.randn(1000,500)
+ini_value_w4 = rng.randn(500,30)
+w_list_3_layer = [ini_value_w1, ini_value_w2, ini_value_w3, np.transpose(ini_value_w3), \
+np.transpose(ini_value_w2), np.transpose(ini_value_w1)]
+w_list_4_layer = [ini_value_w1, ini_value_w2, ini_value_w3, ini_value_w4, \
+np.transpose(ini_value_w4),np.transpose(ini_value_w3),np.transpose(ini_value_w2),np.transpose(ini_value_w1)]
 
-ini_value_b1 = rng.randn(8,1)
-ini_value_b2 = rng.randn(6,1)
-ini_value_b3 = rng.randn(4,1)
-ini_value_b4 = rng.randn(2,1)
-ini_value_b5 = rng.randn(4,1)
-ini_value_b6 = rng.randn(6,1)
-ini_value_b7 = rng.randn(8,1)
-ini_value_b8 = rng.randn(10,1)
+ini_value_b1 = rng.randn(2000,1)
+ini_value_b2 = rng.randn(1000,1)
+ini_value_b3 = rng.randn(500,1)
+ini_value_b4 = rng.randn(30,1)
+ini_value_b5 = rng.randn(500,1)
+ini_value_b6 = rng.randn(1000,1)
+ini_value_b7 = rng.randn(2000,1)
+ini_value_b8 = rng.randn(784,1)
 b_list_3_layer = [ini_value_b1, ini_value_b2, ini_value_b3, ini_value_b6, ini_value_b7, ini_value_b8]
 b_list_4_layer = [ini_value_b1, ini_value_b2, ini_value_b3, ini_value_b4, ini_value_b5, ini_value_b6, ini_value_b7, ini_value_b8]
 
-simulate_data = rng.randn(10,simulate_data_num)  # 50 data points
+simulate_data = rng.randn(784,simulate_data_num)  # 50 data points
 
-total_cost_record_3_lay, w_list_output_3_lay, b_list_output_3_lay = fine_tuning(w_list_3_layer, b_list_3_layer, simulate_data, num_batch, training_steps, lr)
+#total_cost_record_3_lay, w_list_output_3_lay, b_list_output_3_lay = fine_tuning(w_list_3_layer, b_list_3_layer, simulate_data, num_batch, training_steps, lr)
 total_cost_record_4_lay, w_list_output_4_lay, b_list_output_4_lay = fine_tuning(w_list_4_layer, b_list_4_layer, simulate_data, num_batch, training_steps, lr)
 
+end_time = time.time()
 
+print(end_time-start_time)
 
 fig, ax = plt.subplots()
-ax.scatter(range(len(total_cost_record_3_lay)),total_cost_record_3_lay)
+
+#ax.scatter(range(len(total_cost_record_3_lay)),total_cost_record_3_lay)
+
 ax.scatter(range(len(total_cost_record_4_lay)),total_cost_record_4_lay)
 plt.show()
--- fine_tuning.py
+++ fine_tuning.py
@@ -10,7 +10,7 @@ def fine_tuning(w_list, b_list, data, num_batch, training_steps, lr):
      one_batch_size = int(data_num / num_batch)
      total_cost_record = []
 
-     if len(w_list) == 3:
+     if len(w_list) == 6:
           for count in range(num_batch-1):
                batch_data = data[:,count*one_batch_size:(count+1)*one_batch_size]
                cost_record, w_list_output, b_list_output = fine_tuning_3_one_batch(w_list, b_list, batch_data, training_steps, lr)
@@ -22,7 +22,7 @@ def fine_tuning(w_list, b_list, data, num_batch, training_steps, lr):
           batch_data = data[:,(num_batch-1)*one_batch_size:]
           cost_record, w_list_output, b_list_output = fine_tuning_3_one_batch(w_list, b_list, batch_data, training_steps, lr)
           total_cost_record = total_cost_record + cost_record
-     elif len(w_list) == 4:
+     elif len(w_list) == 8:
           for count in range(num_batch-1):
                batch_data = data[:,count*one_batch_size:(count+1)*one_batch_size]
                cost_record, w_list_output, b_list_output = fine_tuning_4_one_batch(w_list, b_list, batch_data, training_steps, lr)
@@ -47,6 +47,9 @@ def fine_tuning_3_one_batch(w_list, b_list, batch_data, training_steps, lr):
      ini_value_w1 = w_list[0]
      ini_value_w2 = w_list[1]
      ini_value_w3 = w_list[2]
+     ini_value_w4 = w_list[3]
+     ini_value_w5 = w_list[4]
+     ini_value_w6 = w_list[5]
      ini_value_b1 = np.tile(b_list[0],(1,batch_data_num))
      ini_value_b2 = np.tile(b_list[1],(1,batch_data_num))
      ini_value_b3 = np.tile(b_list[2],(1,batch_data_num))
@@ -59,6 +62,9 @@ def fine_tuning_3_one_batch(w_list, b_list, batch_data, training_steps, lr):
      w1 = theano.shared(ini_value_w1, name="w1")
      w2 = theano.shared(ini_value_w2, name="w2")
      w3 = theano.shared(ini_value_w3, name="w3")
+     w4 = theano.shared(ini_value_w4, name="w4")
+     w5 = theano.shared(ini_value_w5, name="w5")
+     w6 = theano.shared(ini_value_w6, name="w6")
 
      b1 = theano.shared(ini_value_b1, name="b1")
      b2 = theano.shared(ini_value_b2, name="b2")
@@ -66,20 +72,20 @@ def fine_tuning_3_one_batch(w_list, b_list, batch_data, training_steps, lr):
      b4 = theano.shared(ini_value_b4, name="b4")
      b5 = theano.shared(ini_value_b5, name="b5")
      b6 = theano.shared(ini_value_b6, name="b6")
-     para = [b1, b2, b3, b4, b5, b6, w1, w2, w3]
+     para = [b1, b2, b3, b4, b5, b6, w1, w2, w3, w4, w5, w6]
 
      step1_result = T.dot(T.transpose(w1),data_sym) + b1 > 0
      step2_result = T.dot(T.transpose(w2),step1_result) + b2 > 0
      step3_result = T.dot(T.transpose(w3),step2_result) + b3 > 0
-     step4_result = T.dot(w3,step3_result) + b4 > 0
-     step5_result = T.dot(w2,step4_result) + b5 > 0
-     result = T.dot(w1,step5_result) + b6
+     step4_result = T.dot(T.transpose(w4),step3_result) + b4 > 0
+     step5_result = T.dot(T.transpose(w5),step4_result) + b5 > 0
+     result = T.dot(T.transpose(w6),step5_result) + b6
 
 
 
      diff = result - data_sym # Cross-entropy loss function
      cost = (diff ** 2).mean() # no regulization, this will return the mean of the matrix
-     gb1, gb2, gb3, gb4, gb5, gb6, gw1, gw2, gw3 = T.grad(cost, para)
+     gb1, gb2, gb3, gb4, gb5, gb6, gw1, gw2, gw3, gw4, gw5, gw6 = T.grad(cost, para)
 
      # Compile
 
@@ -87,7 +93,8 @@ def fine_tuning_3_one_batch(w_list, b_list, batch_data, training_steps, lr):
                inputs=[data_sym],
                outputs=[cost],
                updates=((w1, w1 - lr*gw1),(w2, w2 - lr*gw2), \
-               	(w3, w3 - lr*gw3), (b1, b1 - lr*gb1), \
+               	(w3, w3 - lr*gw3), (w4, w4 - lr*gw4), (w5, w5 - lr*gw5), \
+                    (w6, w6 - lr*gw6), (b1, b1 - lr*gb1), \
                	(b2, b2 - lr*gb2), (b3, b3 - lr*gb3), \
                	(b4, b4 - lr*gb4), (b5, b5 - lr*gb5), \
                	(b6, b6 - lr*gb6)))
@@ -99,7 +106,7 @@ def fine_tuning_3_one_batch(w_list, b_list, batch_data, training_steps, lr):
      for i in range(training_steps):
      	cost = train(batch_data)
      	cost_record.append(cost)
-     w_list_output = [w1.get_value(),w2.get_value(),w3.get_value()]
+     w_list_output = [w1.get_value(),w2.get_value(),w3.get_value(),w4.get_value(),w5.get_value(),w6.get_value()]
      b_list_output = [b1.get_value()[:,[0]],b2.get_value()[:,[0]],b3.get_value()[:,[0]],b4.get_value()[:,[0]],b5.get_value()[:,[0]],b6.get_value()[:,[0]]]
      return cost_record, w_list_output, b_list_output
 
@@ -111,6 +118,10 @@ def fine_tuning_4_one_batch(w_list, b_list, batch_data, training_steps, lr):
      ini_value_w2 = w_list[1]
      ini_value_w3 = w_list[2]
      ini_value_w4 = w_list[3]
+     ini_value_w5 = w_list[4]
+     ini_value_w6 = w_list[5]
+     ini_value_w7 = w_list[6]
+     ini_value_w8 = w_list[7]
      ini_value_b1 = np.tile(b_list[0],(1,batch_data_num))
      ini_value_b2 = np.tile(b_list[1],(1,batch_data_num))
      ini_value_b3 = np.tile(b_list[2],(1,batch_data_num))
@@ -126,6 +137,10 @@ def fine_tuning_4_one_batch(w_list, b_list, batch_data, training_steps, lr):
      w2 = theano.shared(ini_value_w2, name="w2")
      w3 = theano.shared(ini_value_w3, name="w3")
      w4 = theano.shared(ini_value_w4, name="w4")
+     w5 = theano.shared(ini_value_w5, name="w5")
+     w6 = theano.shared(ini_value_w6, name="w6")
+     w7 = theano.shared(ini_value_w7, name="w7")
+     w8 = theano.shared(ini_value_w8, name="w8")
 
 
      b1 = theano.shared(ini_value_b1, name="b1")
@@ -136,21 +151,21 @@ def fine_tuning_4_one_batch(w_list, b_list, batch_data, training_steps, lr):
      b6 = theano.shared(ini_value_b6, name="b6")
      b7 = theano.shared(ini_value_b7, name="b7")
      b8 = theano.shared(ini_value_b8, name="b8")
-     para = [b1, b2, b3, b4, b5, b6, b7, b8, w1, w2, w3, w4]
+     para = [b1, b2, b3, b4, b5, b6, b7, b8, w1, w2, w3, w4, w5, w6, w7, w8]
 
      step1_result = T.dot(T.transpose(w1),data_sym) + b1 > 0
      step2_result = T.dot(T.transpose(w2),step1_result) + b2 > 0
      step3_result = T.dot(T.transpose(w3),step2_result) + b3 > 0
      step4_result = T.dot(T.transpose(w4),step3_result) + b4 > 0
-     step5_result = T.dot(w4,step4_result) + b5 > 0
-     step6_result = T.dot(w3,step5_result) + b6 > 0
-     step7_result = T.dot(w2,step6_result) + b7 > 0
-     result = T.dot(w1,step7_result) + b8
+     step5_result = T.dot(T.transpose(w5),step4_result) + b5 > 0
+     step6_result = T.dot(T.transpose(w6),step5_result) + b6 > 0
+     step7_result = T.dot(T.transpose(w7),step6_result) + b7 > 0
+     result = T.dot(T.transpose(w8),step7_result) + b8
 
 
      diff = result - data_sym # Cross-entropy loss function
      cost = (diff ** 2).mean() # no regulization, this will return the mean of the matrix
-     gb1, gb2, gb3, gb4, gb5, gb6, gb7, gb8, gw1, gw2, gw3, gw4 = T.grad(cost, para)
+     gb1, gb2, gb3, gb4, gb5, gb6, gb7, gb8, gw1, gw2, gw3, gw4, gw5, gw6, gw7, gw8 = T.grad(cost, para)
 
      # Compile
 
@@ -158,7 +173,10 @@ def fine_tuning_4_one_batch(w_list, b_list, batch_data, training_steps, lr):
                inputs=[data_sym],
                outputs=[cost],
                updates=((w1, w1 - lr*gw1),(w2, w2 - lr*gw2), \
-                    (w3, w3 - lr*gw3), (w4, w4 - lr*gw4), (b1, b1 - lr*gb1), \
+                    (w3, w3 - lr*gw3), (w4, w4 - lr*gw4), \
+                     (w5, w5 - lr*gw5), (w6, w6 - lr*gw6), \
+                     (w7, w7 - lr*gw7), (w8, w8 - lr*gw8), \
+                    (b1, b1 - lr*gb1), \
                     (b2, b2 - lr*gb2), (b3, b3 - lr*gb3), \
                     (b4, b4 - lr*gb4), (b5, b5 - lr*gb5), \
                     (b6, b6 - lr*gb6), (b7, b7 - lr*gb7), (b8, b8 - lr*gb8)))
@@ -170,7 +188,8 @@ def fine_tuning_4_one_batch(w_list, b_list, batch_data, training_steps, lr):
      for i in range(training_steps):
           cost = train(batch_data)
           cost_record.append(cost)
-     w_list_output = [w1.get_value(),w2.get_value(),w3.get_value(),w4.get_value()]
+     w_list_output = [w1.get_value(),w2.get_value(),w3.get_value(),w4.get_value(), \
+     w5.get_value(),w6.get_value(),w7.get_value(),w8.get_value()]
      b_list_output = [b1.get_value()[:,[0]],b2.get_value()[:,[0]],b3.get_value()[:,[0]],b4.get_value()[:,[0]], \
      b5.get_value()[:,[0]],b6.get_value()[:,[0]],b7.get_value()[:,[0]],b8.get_value()[:,[0]]]
      return cost_record, w_list_output, b_list_output
